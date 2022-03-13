ed0a0f4bd5b880750b1e4bdfa732368e7fc9221b
yousefShaaban42 <92793137+yousefShaaban42@users.noreply.github.com>
2022-03-13 22:08:36 +0200

    Initial commit

---
+++ app/src/main/java/com/yousef/el_mufassir/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yousef.el_mufassir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
