322b54b7466294a288109cc88e3ca578c88dcb69
yousefShaaban42 <92793137+yousefShaaban42@users.noreply.github.com>
2022-03-16 12:47:00 +0200

    update proguard-rules.pro

--- app/src/main/java/com/yousef/el_mufassir/functions/MyAlarm.java
+++ app/src/main/java/com/yousef/el_mufassir/functions/MyAlarm.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.yousef.el_mufassir.databse.Repository;
+import com.yousef.el_mufassir.model.Constants;
 
 public class MyAlarm extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Repository repository=new Repository(context);
         repository.getNewAya();
+        Constants constants=Constants.newInstance();
+        repository.saveInt(constants.CHECK_ALARM,0);
     }
 }
